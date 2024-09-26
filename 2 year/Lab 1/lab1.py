87c6514339fe2554231f766f9dd883700d34d0d0
diekinari <ushakow.email@gmail.com>
2024-09-26 15:28:21 +0300

    upd

--- 2 year/Lab 1/lab1.py
+++ 2 year/Lab 1/lab1.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import math
+from math import *
 
 root = Tk()
 c = Canvas(root, width=600, height=600, bg="gray")
@@ -8,7 +8,7 @@ c.pack()
 ball = c.create_oval(100, 100, 500, 500, fill='lightblue')
 dot = c.create_oval(100, 250, 110, 260, fill='red')
 
-center_x, center_y = 300, 300
+centralX, centralY = 300, 300
 radius = 200
 angle = 0
 speed = 0.01
@@ -38,12 +38,10 @@ def decrease_speed():
 
 def motion():
     global angle
-
     # directionRight = True
-    x = center_x + radius * math.cos(angle)
-    y = center_y + radius * math.sin(angle)
+    x = centralX + radius * cos(angle)
+    y = centralY + radius * sin(angle)
     c.coords(dot, x - 5, y - 5, x + 5, y + 5)
-
     if directionRight:
         angle += speed
     else:
